2c2f154f14f91b2e6f01f15d3b9b715d882587a5
TsunenoriHenna <tsunenori.henna@dragon.jp>
2024-11-03 21:12:08 +0900

    他のCloudFunctionsにpostする処理追加

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import functions_framework
+import requests
 
 # HTTPトリガ
 @functions_framework.http
 def main(request):
+	requests.post("https://asia-northeast1-android-picture-1816c.cloudfunctions.net/cloudfunction1")
         print("Hello World")
         return "TEST HELLO"
