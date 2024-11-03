51c5d5000e0130fe15049a95e058c41d991830a6
TsunenoriHenna <tsunenori.henna@dragon.jp>
2024-11-03 21:49:56 +0900

    修正

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import requests
 @functions_framework.http
 def main(request):
         response = requests.post("https://asia-northeast1-android-picture-1816c.cloudfunctions.net/cloudfunction1")
-	print(f"{response.status_code}")
+        print(f"{response.status_code}")
         print(f"{response.text}")
         return "TEST HELLO"
