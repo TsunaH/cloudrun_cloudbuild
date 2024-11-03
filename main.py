24ebb1da70972a83f05278a1abe3278d98f3edbd
TsunenoriHenna <tsunenori.henna@dragon.jp>
2024-11-03 21:19:41 +0900

    不具合修正

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import requests
 # HTTPトリガ
 @functions_framework.http
 def main(request):
-	requests.post("https://asia-northeast1-android-picture-1816c.cloudfunctions.net/cloudfunction1")
+        requests.post("https://asia-northeast1-android-picture-1816c.cloudfunctions.net/cloudfunction1")
         print("Hello World")
         return "TEST HELLO"
